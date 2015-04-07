799794bf70f7612e50e25bf8cfdba719a220baa7
Jakub Červenka <redwormik@gmail.com>
2015-04-07 23:58:10 +0200

    Better layout styles

--- application/components/RendererBox.jsx
+++ application/components/RendererBox.jsx
@@ -10,18 +10,24 @@ var RendererBox = React.createClass({
 		Ambidex.mixinCreators.connectStoresToLocalState(["Components", "Data"])
 	],
 	render: function () {
+		var paneStyles = {
+			width: "50vw",
+			height: "100vh",
+			padding: 8,
+			boxSizing: "border-box"
+		};
 		return (
-			<div style={{ height: "100%" }}>
-				<div style={{ width: "49%", float: "right", height: "100%" }}>
-					<Renderer data={ this.state.data } components={ this.state.components } />
-				</div>
-				<div style={{ width: "49%", float: "left", height: "100%" }}>
+			<div>
+				<div style={{ ...paneStyles, float: "left" }}>
 					<JsonEditBox data={ this.state.components }
 						onChange={ this.getRefluxAction("updateComponents") }
 						style={{ width: "100%", height: "50%" }} />
 					<JsonEditBox data={ this.state.data }
 						onChange={ this.getRefluxAction("updateData") }
-						style={{ width: "100%", height: "50%" }}  />
+						style={{ width: "100%", height: "50%" }} />
+				</div>
+				<div style={{ ...paneStyles, float: "right", overflow: "auto" }}>
+					<Renderer data={ this.state.data } components={ this.state.components } />
 				</div>
 				<div style={{ clear: "both" }} />
 			</div>
